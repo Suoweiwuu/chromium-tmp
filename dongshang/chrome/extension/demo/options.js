f9d3e0658e93489ec5dee1bf696f04dd00edb3b0
gclxry <gclxry@gmail.com>
2022-05-22 15:39:03 +0800

    添加注入 js 测试代码

--- dongshang/chrome/extension/demo/options.js
+++ dongshang/chrome/extension/demo/options.js
@@ -22,10 +22,36 @@ function GetAllFrames() {
   );
 }
 
+function ShowTab() {
+  let info ="";
+  info += location.href + "\n";
+  info += document.title+ "\n";
+  console.log(info);
+}
+
+function ExecuteScript() {
+  chrome.tabs.query(
+    {currentWindow: true},
+    function(tabs){
+      
+      for(let i = 0; i < tabs.length; i++) {
+        chrome.scripting.executeScript(
+        {
+          target: {tabId: tabs[i].id},
+          func: ShowTab,
+        },
+        () => {  });
+      
+      }
+    }
+  );
+}
+
 // Add a button to the page for each supplied color
 function constructOptions(buttonColors) {
   document.getElementById("all_tabs_button").onclick = GetAllTabs;
   document.getElementById("all_frames_button").onclick = GetAllFrames;
+  document.getElementById("execute_script").onclick = ExecuteScript;
 }
 
 // Initialize the page by constructing the color options
