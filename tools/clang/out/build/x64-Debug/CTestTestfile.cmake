2fb2c31614c87b60f254b55cb4c57e81c96d9d3e
CococoloSimida <1402872735@qq.com>
2024-02-05 20:49:25 +0800

    回滚

--- tools/clang/out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/project/src/tools/clang
-# Build directory: F:/project/src/tools/clang/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
